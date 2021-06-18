fba5e7462a0f348539e9fd860c7936ef74ab6ec9
jpatzi10 <jpatzi@adsib.gob.bo>
2021-06-18 08:55:42 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  src =
+    'https://repositorio.cepal.org/bitstream/handle/11362/40825/85/1601260BP_Bolivia_es.pdf';
+}
